b114fc4b7209f0829e7fede052e3b8211a01b2ef
mathd1as <matheus.vex@gmail.com>
2024-05-11 19:45:12 -0300

    feat: create restaurant schema

--- src/routes/restaurant/create-restaurant.route.ts
+++ src/routes/restaurant/create-restaurant.route.ts
@@ -1,20 +1,21 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { CreateRestaurantService } from "../../services/create-restaurant.service";
-import { createRestaurant } from "../../interfaces/create-restaurante.interface";
-
+import { CreateRestaurant } from "../../interfaces/create-restaurante.interface";
+import { createRestaurantBody } from "./restaurant-schema.consts";
 
 const schema = {
-  body: {
-    type: 'array',
-    items: {}
-  }
+  body: createRestaurantBody
 }
+
 export const CreateRestaurantRoute = async (app: FastifyInstance) => {
- app.post('/restaurant', async (request: FastifyRequest<{ Body: createRestaurant }>, response: FastifyReply) => {
-      const { name, address, picture, openingTime, closingTime } = request.body
-      const restaurantProps = { name, address, picture, openingTime, closingTime }
-      const createRestaurantService = new CreateRestaurantService()
-      const result = createRestaurantService.handle(restaurantProps);
-      return response.status(200).send(result)
+  app.post('/restaurant', { schema }, async (request: FastifyRequest<{ Body: CreateRestaurant }>, response: FastifyReply) => {
+    const { name, address, picture, openingTime, closingTime } = request.body
+    const restaurantProps = { name, address, picture, openingTime, closingTime }
+
+    const createRestaurantService = new CreateRestaurantService()
+    
+    const result = createRestaurantService.handle(restaurantProps);
+
+    return response.status(200).send(result)
   })
 }
---
+++ src/routes/restaurant/restaurant-schema.consts.ts
@@ -0,0 +1,21 @@
+export const createRestaurantBody = {
+    type: 'object',
+    required: ['name', 'address', 'picture', 'openingTime', 'closingTime'],
+    properties: {
+      name: {
+        type: 'string'
+      },
+      address: {
+        type: 'string'
+      },
+      picture: {
+        type: 'string'
+      },
+      openingTime: {
+        type: 'string'
+      },
+      closingTime: {
+        type: 'string'
+      }
+    }
+}
