5ce860bcdc511058251c5a3fea4e7933cb6873fe
mathd1as <matheus.vex@gmail.com>
2024-05-13 14:28:02 -0300

    refactor: route name

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,9 @@
 import {app} from '../server.ts'
-import { CreateProductRoute } from './product/create-product.route.ts'
+import { ProductRoutes } from './product/product.route.ts'
 import { RestaurantRoutes } from './restaurant/restaurant.routes.ts'
 
 
 export const routes = async () => {
     app.register(RestaurantRoutes)
-    app.register(CreateProductRoute)
+    app.register(ProductRoutes)
 }
