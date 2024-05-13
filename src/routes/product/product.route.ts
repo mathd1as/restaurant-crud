fbd0679d1a3560b20127513cc8316262d0ed8381
mathd1as <matheus.vex@gmail.com>
2024-05-13 14:27:40 -0300

    refactor: product file name

--- src/routes/product/product.route.ts
+++ src/routes/product/product.route.ts
@@ -1,20 +1,19 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
-import { CreateProduct } from "../../interfaces/create-product.interface";
-import { CreateProductService } from "../../services/create-product.service";
 import { createProductBody } from "./product-schema.consts";
+import { ProductService } from "../../services/product.service";
+import { CreateProduct } from "../../interfaces/product.interface";
 
 const schema = {
   body: createProductBody
 }
 
-export const CreateProductRoute = async (app: FastifyInstance) => {
+export const ProductRoutes = async (app: FastifyInstance) => {
+  const productService = new ProductService()
   app.post('/product', { schema }, async (request: FastifyRequest<{ Body: CreateProduct }>, response: FastifyReply) => {
     const { name, picture, category, price, promotionActive, restaurantId } = request.body
     const productProps = { name, category, picture, price, promotionActive, restaurantId }
 
-    const createProductService = new CreateProductService()
-    
-    const result = await createProductService.handle(productProps);
+    const result = await productService.create(productProps);
 
     return response.status(200).send(result)
   })
