bafb4146bf8d1b45d73dde10e753eeb7c7eae345
Matheus Dias <matheus.vex@gmail.com>
2024-05-31 18:03:28 -0300

    chore: add eslint and prettier setup

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  {
+    languageOptions: { globals: globals.browser,  parserOptions: {
+      project: ["./tsconfig.json"]
+  } },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
